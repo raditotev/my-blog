5b96e3a3d7487a6c47ce82914fd860a392cbdd66
Radi Totev <raditotev@gmail.com>
2022-05-09 22:11:08 +0300

    Add view post page

---
+++ pages/posts/[id].js
@@ -0,0 +1,62 @@
+import ReactMarkdown from 'react-markdown';
+
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Layout from '../../components/Layout';
+import CodeBlock from '../../components/Codeblock';
+import TimeStamp from '../../components/TimeStamp';
+import Link from 'next/link';
+
+export const getStaticPaths = async () => {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      post: JSON.parse(postData),
+    },
+  };
+};
+
+const Post = ({ post }) => {
+  return (
+    <Layout>
+      <div className="post">
+        <header>
+          <h1>{post.title}</h1>
+          <TimeStamp date={post.date} updated={post.updated}></TimeStamp>
+        </header>
+        <ReactMarkdown components={CodeBlock}>{post.markdown}</ReactMarkdown>
+        <footer align="right">
+          <Link href="/">
+            <a>⬅ Back to homepage</a>
+          </Link>
+        </footer>
+      </div>
+      <style jsx>{`
+        .post {
+          width: 100%;
+        }
+        header {
+          text-align: center;
+          margin-bottom: 3rem;
+        }
+        .post h1 {
+          margin-bottom: 0.2rem;
+        }
+
+        a {
+          color: black;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default Post;
