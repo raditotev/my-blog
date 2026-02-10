d01a598f88b1e8b3a7e017d2f56878d6d7b0902f
Radi Totev <raditotev@gmail.com>
2026-02-10 16:53:01 +0200

    Fixed plausible endpoint to point to self-hosted server

--- components/PlausibleAnalytics.js
+++ components/PlausibleAnalytics.js
@@ -3,13 +3,15 @@ import { init } from '@plausible-analytics/tracker';
 
 const PLAUSIBLE_DOMAIN =
   process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN || 'blog.radi.pro';
+const PLAUSIBLE_ENDPOINT =
+  process.env.NEXT_PUBLIC_PLAUSIBLE_ENDPOINT || 'https://analytics.radi.pro/api/event';
 
 export default function PlausibleAnalytics() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       init({
         domain: PLAUSIBLE_DOMAIN,
-        apiHost: 'https://analytics.radi.pro',
+        endpoint: PLAUSIBLE_ENDPOINT,
       });
     }
   }, []);
